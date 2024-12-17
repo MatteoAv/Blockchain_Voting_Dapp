b1be2cfe18811a3c0ffb4ae14e8c10a8c83d392c
MatteoAv <matteoavella8@gmail.com>
2024-12-17 11:32:01 +0100

    Modifica contratto e aggiunta test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -9,6 +9,10 @@ contract Election{
         uint voteCount;
     }
     
+    //Il mapping tiene traccia degli utenti che hanno già votato, ad ogni indirizzo di un account associa un valore booleano
+    //Il valore booleano è settato di default a FALSE
+    mapping(address => bool) public voters;
+
     //Il mapping tiene traccia dei candidati, ad ogni id associa un candidato univoco
     mapping(uint => Candidate) public candidates;
 
@@ -21,9 +25,27 @@ contract Election{
         addCandidate("Candidate 2");
     }
 
-    //la funzione incrementa di 1 il numero di candidati nel mapping e aggiunge un nuovo candidato passando id(il contatore viene usato come id), nome e numero di voti
+    //La funzione incrementa di 1 il numero di candidati nel mapping e aggiunge un nuovo candidato passando id(il contatore viene usato come id), nome e numero di voti
     function addCandidate (string memory _name) private{
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    /*
+      La funzione setta a true il valore del votante (per indicare che ha votato) ed incrementa di 1 
+      il contatore del candidato il cui id è stato passato come parametro.
+      La funzione deve essere pubblica, chiunque deve poter votare
+    */
+    function vote (uint _candidateId) public {
+
+        /* Con require se la condizione non è verificata la funzione si interrompe immediatamente */
+
+        //controlla che l'utente non abbia già votato
+        require(!voters[msg.sender]);
+        //controlla che il candidato selezionato sia un candidato valido
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount ++;        
     }
 }
--- test/testElection.js
+++ test/testElection.js
@@ -5,11 +5,11 @@ describe("Election Contract", function () {
   let owner;
 
   beforeEach(async function () {
-    // Ottieni il contratto e il proprietario
+    //ottieni il contratto e il proprietario
     const Election = await ethers.getContractFactory("Election");
     [owner] = await ethers.getSigners();
 
-    // Deploy del contratto
+    //deploy del contratto
     election = await Election.deploy();
   });
 
@@ -17,6 +17,7 @@ describe("Election Contract", function () {
     const candidateCount = await election.candidatesCount();
     expect(candidateCount).to.equal(2);
   });
+
 
   it("initializes the candidates with the correct values", async function () {
     const candidate1 = await election.candidates(1);
@@ -29,4 +30,53 @@ describe("Election Contract", function () {
     expect(candidate2.name).to.equal("Candidate 2");
     expect(candidate2.voteCount).to.equal(0);
   });
+
+
+  it("allows a voter to cast a vote", async function () {
+    const candidateId = 1;
+
+    //chiama la funzione per votare
+    await election.vote(candidateId);
+
+    //controlla se il valore booleano associato all'indirizzo del votante è true
+    const hasVoted = await election.voters(owner.address);
+    expect(hasVoted).to.be.true;
+
+    //controlla se il contatore dei voti del candidato votato è stato incrementato correttamente
+    const candidate  = await election.candidates(candidateId);
+    expect(candidate.voteCount).to.equal(1);
+  });  
+
+
+  it("throws an exception for invalid candidates", async function () {
+    const invalidCandidateId = 99;
+  
+    //prova a votare per un candidato non valido e verifica che venga generata un'eccezione
+    await expect(election.vote(invalidCandidateId)).to.be.reverted;
+  
+    //controlla che i voti dei candidati validi non siano cambiati
+    const candidate1 = await election.candidates(1);
+    expect(candidate1.voteCount).to.equal(0, "candidate 1 did not receive any votes");
+  
+    const candidate2 = await election.candidates(2);
+    expect(candidate2.voteCount).to.equal(0, "candidate 2 did not receive any votes");
+  });
+
+
+  it("does not allow a voter to vote more than once", async function () {
+    const candidateId = 1;
+  
+    //la prima chiamata per votare deve andare a buon fine
+    await election.vote(candidateId);
+  
+    //la seconda chiamata deve fallire
+    await expect(election.vote(candidateId)).to.be.reverted;
+  
+    //controlla che i voti del candidato rimangono invariati
+    const candidate = await election.candidates(candidateId);
+    expect(candidate.voteCount).to.equal(1, "candidate vote count should not change after a repeated vote attempt");
+  });
+    
+  
+
 });
